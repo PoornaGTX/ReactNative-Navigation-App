f8f3d7e75e0d0fed145af6121e897b326f7c9242
Poorna Ranasinghe <onlinepoorna@gmail.com>
2022-09-06 14:18:58 +0530

    Land Screen

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,39 @@
+import { View, Pressable, Text, StyleSheet, Platform } from "react-native";
+
+const CategoryGridTile = ({ title, color }) => {
+  return (
+    <View style={[styles.girdItem, { backgroundColor: color }]}>
+      <Pressable android_ripple={{ color: "#ccc" }} style={styles.buttonStyle}>
+        <View style={styles.innerContainer}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+};
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  girdItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  buttonStyle: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import { View, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+//optional
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
