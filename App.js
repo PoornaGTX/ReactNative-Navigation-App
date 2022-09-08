848dbd07c1e3f4c14e6cee5b9c4062b980040cdb
Poorna Ranasinghe <onlinepoorna@gmail.com>
2022-09-08 09:03:46 +0530

    Add navigation

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import { NavigationContainer } from "@react-navigation/native";
+//entire app eka navigateble wenna ona nisa app js eka import karnawa
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
 
-CategoriesScreen;
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
