13982d55db1dfa65917c9c263497e43ced601204
Poorna Ranasinghe <onlinepoorna@gmail.com>
2022-09-09 13:31:47 +0530

    add nested navigation

--- App.js
+++ App.js
@@ -1,16 +1,63 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, Button } from "react-native";
+import "react-native-gesture-handler";
+
+import { Ionicons } from "@expo/vector-icons";
 
 import { NavigationContainer } from "@react-navigation/native";
 //entire app eka navigateble wenna ona nisa app js eka import karnawa
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import CategoriesScreen from "./screens/CategoriesScreen";
 import MealsOverViewScreen from "./screens/MealsOverViewScreen";
 import MealDeatailScreen from "./screens/MealDeatailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" }, //title backgorund color
+        headerTintColor: "white", //title color
+        sceneContainerStyle: {
+          //stack eke contentStyle kiyala use karata sceneContainerStyle thami drawer eke use karanna ona
+          backgroundColor: "#351401",
+        },
+        drawerContentStyle: {
+          backgroundColor:
+            "#351401" /* change the backgroud color of side bar */,
+        },
+        drawerInactiveTintColor: "white",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -28,25 +75,11 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{
-              title: "All Categories",
-            }}
+            name="Drawer"
+            component={DrawerNavigator}
+            options={{ headerShown: false }}
           />
-          <Stack.Screen
-            name="MealsOverView"
-            component={MealsOverViewScreen}
-            // options={({ route, navigation }) => {
-            //   //CategoriesScreen eka navigation prop eke data eka methanin access karanna puluwan
-            //   const catID = route.params.categoryId;
-
-            //   return {
-            //     title: catID,
-            //   }; //return options like we use in Stack.Navigator
-            // }}
-          />
-
+          <Stack.Screen name="MealsOverView" component={MealsOverViewScreen} />
           <Stack.Screen name="MealDetail" component={MealDeatailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
